fe04a5d9e024eb10fba0a7679826941c2feaca7e
alexflen <77319251+alexflen@users.noreply.github.com>
2021-11-26 12:59:41 +0300

    Introduced classes EnrollSportsman and StartGroup

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,14 +1,16 @@
 package ru.emkn.kotlin.sms
 
-open class Sportsman(surname: String, name: String, birthYear: Int, category: String? = null) {
+open class Sportsman(surname: String, name: String, birthYear: Int, collective: String, category: String? = null) {
     val surname: String
     val name: String
     val birthYear: Int
+    var collective: String
     var category: String?
     init {
         this.surname = surname
         this.name = name
         this.birthYear = birthYear
+        this.collective = collective
         this.category = category
     }
 
@@ -17,8 +19,20 @@ open class Sportsman(surname: String, name: String, birthYear: Int, category: St
     }
 }
 
-class StartSportsman(surname: String, name: String, birthYear: Int, number: Int, start: String, category: String? = null) :
-    Sportsman(surname, name, birthYear, category) {
+class EnrollSportsman(surname: String, name: String, birthYear: Int, collective: String, category: String? = null, desiredGroup: String? = null):
+    Sportsman(surname, name, birthYear, collective, category)  {
+    val desiredGroup: String?
+    init {
+        this.desiredGroup = desiredGroup
+    }
+
+    override fun toString(): String {
+        return "${super.toString()},${desiredGroup?: ""},$collective"
+    }
+}
+
+class StartSportsman(surname: String, name: String, birthYear: Int, number: Int, start: String, collective: String, category: String? = null) :
+    Sportsman(surname, name, birthYear, collective, category) {
     val start: String
     val number: Int
     init {
@@ -27,11 +41,18 @@ class StartSportsman(surname: String, name: String, birthYear: Int, number: Int,
     }
 
     override fun toString(): String {
-        return "$number,${super.toString()},$start"
+        return "$number,${super.toString()},$collective,$start"
     }
 }
 
-
+class StartGroup(name: String, participants: List<StartSportsman>) {
+    val name: String
+    val participants: List<StartSportsman>
+    init {
+        this.name = name
+        this.participants = participants
+    }
+}
 
 
 fun main(args: Array<String>) {
