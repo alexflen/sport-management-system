3e8d2fb95dbf10519453e80488ab883fc168b4a7
alexflen <77319251+alexflen@users.noreply.github.com>
2021-11-26 12:27:51 +0300

    Introduced classes Sportsman and StartSportsman

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,5 +1,39 @@
 package ru.emkn.kotlin.sms
 
+open class Sportsman(surname: String, name: String, birthYear: Int, category: String? = null) {
+    val surname: String
+    val name: String
+    val birthYear: Int
+    var category: String?
+    init {
+        this.surname = surname
+        this.name = name
+        this.birthYear = birthYear
+        this.category = category
+    }
+
+    override fun toString(): String {
+        return "$surname,$name,$birthYear,${category?: ""}"
+    }
+}
+
+class StartSportsman(surname: String, name: String, birthYear: Int, number: Int, start: String, category: String? = null) :
+    Sportsman(surname, name, birthYear, category) {
+    val start: String
+    val number: Int
+    init {
+        this.start = start
+        this.number = number
+    }
+
+    override fun toString(): String {
+        return "$number,${super.toString()},$start"
+    }
+}
+
+
+
+
 fun main(args: Array<String>) {
     TODO()
 }
