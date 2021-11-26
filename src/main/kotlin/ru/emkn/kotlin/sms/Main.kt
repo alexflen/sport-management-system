09e1d76515f3159cc804a8078f90449c356485d7
alexflen <77319251+alexflen@users.noreply.github.com>
2021-11-26 17:58:35 +0300

    Merged EnrollSportsman and StartSportsman

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -19,41 +19,32 @@ open class Sportsman(surname: String, name: String, birthYear: Int, collective: 
     }
 }
 
-class EnrollSportsman(surname: String, name: String, birthYear: Int, collective: String, category: String? = null, desiredGroup: String? = null):
-    Sportsman(surname, name, birthYear, collective, category)  {
+class StartEnrollSportsman(surname: String, name: String, birthYear: Int, collective: String, desiredGroup: String?):
+    Sportsman(surname, name, birthYear, collective)  {
     val desiredGroup: String?
+    var start: Int? = null
+    var number: Int? = null
     init {
         this.desiredGroup = desiredGroup
     }
 
-    override fun toString(): String {
+    fun toStringEnroll(): String {
         return "${super.toString()},${desiredGroup?: ""},$collective"
     }
-}
 
-class StartSportsman(surname: String, name: String, birthYear: Int, number: Int, start: String, collective: String, category: String? = null) :
-    Sportsman(surname, name, birthYear, collective, category) {
-    val start: String
-    val number: Int
-    init {
-        this.start = start
-        this.number = number
-    }
-
-    override fun toString(): String {
-        return "$number,${super.toString()},$collective,$start"
+    fun toStringStart(): String {
+        return "$$number,${super.toString()},$collective,$start"
     }
 }
 
-class StartGroup(name: String, participants: List<StartSportsman>) {
+class StartGroup(name: String, participants: List<StartEnrollSportsman>) {
     val name: String
-    val participants: List<StartSportsman>
+    val participants: List<StartEnrollSportsman>
     init {
         this.name = name
         this.participants = participants
     }
 }
-
 
 fun main(args: Array<String>) {
     TODO()
