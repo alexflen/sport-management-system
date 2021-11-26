db33b8332ae45476e578b3f5b70da43c3af08324
alexflen <77319251+alexflen@users.noreply.github.com>
2021-11-26 23:50:42 +0300

    Done assignNumbers, assignGroup, divideInGroups

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -20,20 +20,21 @@ open class Sportsman(surname: String, name: String, birthYear: Int, collective: 
 }
 
 class StartEnrollSportsman(surname: String, name: String, birthYear: Int, collective: String, desiredGroup: String?):
-    Sportsman(surname, name, birthYear, collective)  {
+    Sportsman(surname, name, birthYear, collective) {
     val desiredGroup: String?
     var start: Int? = null
     var number: Int? = null
+
     init {
         this.desiredGroup = desiredGroup
     }
 
     fun toStringEnroll(): String {
-        return "${super.toString()},${desiredGroup?: ""},$collective"
+        return "${super.toString()},${desiredGroup ?: ""},$collective"
     }
 
     fun toStringStart(): String {
-        return "$$number,${super.toString()},$collective,$start"
+        return "$number,${super.toString()},$start"
     }
 }
 
@@ -43,6 +44,51 @@ class StartGroup(name: String, participants: List<StartEnrollSportsman>) {
     init {
         this.name = name
         this.participants = participants
+    }
+
+    override fun toString(): String {
+        var result: String = "$name\n"
+        participants.forEach { 
+            result += "${it.toStringStart()}\n"
+        }
+        return result
+    }
+}
+
+class AllGroups(enrolled: List<StartEnrollSportsman>) {
+    val groups: List<StartGroup>
+    init {
+
+    }
+
+    companion object {
+        fun assignNumbers(enrolled: List<StartEnrollSportsman>): List<StartEnrollSportsman> {
+            val shuffled = enrolled.shuffled()
+            val result = mutableListOf<StartEnrollSportsman>()
+            for (i in shuffled.indices) {
+                result.add(shuffled[i])
+                result.last().number = i + 1
+            }
+            return result
+        }
+
+        fun assignGroup(enrolled: List<StartEnrollSportsman>): List<StartEnrollSportsman> {
+            return enrolled.map { it -> StartEnrollSportsman(it.surname, it.name,
+                    it.birthYear, it.collective, it.desiredGroup?: "unspecified") }
+        }
+
+        fun divideInGroups(enrolled: List<StartEnrollSportsman>): List<StartGroup> {
+            return enrolled.groupBy { it.desiredGroup?:
+                throw IllegalArgumentException("Group must be assigned") }.map { StartGroup(it.key, it.value) }
+        }
+
+        fun assignTime(formedGroups: List<StartGroup>): List<StartGroup> {
+            val result = mutableListOf<StartGroup>()
+            for (i in formedGroups.indices) {
+                val currentTime = Time("12:00:00")
+
+            }
+        }
     }
 }
 
