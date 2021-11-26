73e769fc75854a284696f3c9e81b3a41546199e5
74traktor <vikaural84@mail.ru>
2021-11-27 02:55:17 +0500

    Added tests to class time

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -16,6 +16,17 @@ internal class Tests {
         assert(Time2 > Time1)
         assertEquals("10:01:59", (Time2 - Time1).toString())
         assertEquals("23:01:00", (Time2++).toString())
+        assertEquals(1.59361, Time2 / Time1, 1e-3)
+        try {
+            Time1 = Time("24:00:00")
+        } catch (e: IllegalStateException) {
+            assertEquals(e.message, "Incorrect H in time")
+        }
+        try {
+            Time1 - Time2
+        } catch (e: IllegalStateException) {
+            assertEquals(e.message, "More is subtracted from less time")
+        }
     }
 
 }
