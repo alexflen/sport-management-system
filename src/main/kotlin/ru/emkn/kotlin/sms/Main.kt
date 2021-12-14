b3117f425aa2bc9b7490585736597b413f43968e
alexflen <77319251+alexflen@users.noreply.github.com>
2021-12-14 22:41:17 +0300

    A bit of text and csv fields

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -7,13 +7,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -30,9 +31,46 @@ enum class TabTypes(val title: String) {
 @Composable
 @Preview
 fun myApplication() {
-    val currentTab = remember { mutableStateOf(TabTypes.GROUPS) }
-
     MaterialTheme {
+        class TabInfo(var plainText: MutableState<String>,
+                      var plainCSV: MutableState<String>) {
+            var textLines = listOf<String>()
+            var csvLines = listOf<String>()
+
+            fun updatePlainText() {
+                plainText.value = textLines.joinToString("\n")
+            }
+
+            fun updatePlainCSV() {
+                plainCSV.value =  csvLines.joinToString("\n")
+            }
+
+            private fun makeCSVFromText() {
+                csvLines = textLines.map { it.split("""\s+""".toRegex()).joinToString(",") }
+                updatePlainCSV()
+            }
+
+            private fun makeTextFromCSV() {
+                textLines = csvLines.map { it.split(""",+""".toRegex()).joinToString(" ") }
+                updatePlainText()
+            }
+
+            fun updateWhenText(text: String) {
+                textLines = text.split("\n")
+                updatePlainText()
+                makeCSVFromText()
+            }
+
+            fun updateWhenCSV(text: String) {
+                csvLines = text.split("\n")
+                updatePlainCSV()
+                makeTextFromCSV()
+            }
+        }
+
+        val currentTab = remember { mutableStateOf(TabTypes.GROUPS) }
+        val tabInfos = TabTypes.values().associateWith { TabInfo(remember { mutableStateOf("") }, remember { mutableStateOf("") }) }
+
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
                 TabTypes.values().forEach { iterated ->
@@ -41,14 +79,24 @@ fun myApplication() {
                     }) { Text(iterated.title) }
                 }
             }
-            Text(currentTab.value.title)
+            Text(currentTab.value.title, color = Color.Magenta, fontSize = 25.sp)
+            Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                TextField(value = tabInfos[currentTab.value]!!.plainText.value,
+                    onValueChange = { tabInfos[currentTab.value]!!.updateWhenText(it) },
+                    label = { Text("${currentTab.value.title} text info") })
+                TextField(value = tabInfos[currentTab.value]!!.plainCSV.value,
+                    onValueChange = { tabInfos[currentTab.value]!!.updateWhenCSV(it) },
+                    enabled = false,
+                    label = { Text("${currentTab.value.title} csv info") })
+            }
         }
     }
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "Sport Management System",
-        state = rememberWindowState(width = 800.dp, height = 300.dp)
+        state = rememberWindowState(width = 800.dp, height = 700.dp)
     ) {
         myApplication()
     }
